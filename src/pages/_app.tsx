7641a326be5cdcefdc502f99623550db8a416b89
Igor Santos <falaigors@gmail.com>
2021-04-14 00:16:06 -0300

    Configuraçoes Iniciais

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,16 @@
 import { AppProps } from 'next/app';
+import { Header } from '../components/Header/index';
+// import { Provider as NextAuthProvider } from 'next-auth/client'
+
 import '../styles/globals.scss';
 
-function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
